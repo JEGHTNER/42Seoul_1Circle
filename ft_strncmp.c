0acbd75e3d9528c20bd60cf49dfb957c4dcce784
Jeheon Lee <jehelee@c2r5s1.42seoul.kr>
2022-11-16 15:43:19 +0900

    feat : ft_strncmp return value : char -> unsigned char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 16:14:30 by jehelee           #+#    #+#             */
-/*   Updated: 2022/11/16 13:49:48 by jehelee          ###   ########.fr       */
+/*   Updated: 2022/11/16 15:41:57 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *string1, const char *string2, size_t len)
 	while ((*string1 || *string2) && len-- > 0)
 	{
 		if (*string1 != *string2)
-			return (*string1 - *string2);
+			return ((unsigned char)*string1 - (unsigned char )*string2);
 		string1++;
 		string2++;
 	}
