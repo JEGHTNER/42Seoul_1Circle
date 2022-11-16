28e181ae9939302b1dc611444869164b0163469b
Jeheon Lee <jehelee@c2r5s1.42seoul.kr>
2022-11-16 16:20:31 +0900

    feat : ft_calloc fixed : used memset -> bzero

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 12:43:33 by jehelee           #+#    #+#             */
-/*   Updated: 2022/11/16 13:01:06 by jehelee          ###   ########.fr       */
+/*   Updated: 2022/11/16 16:17:24 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*new_space;
 
 	new_space = malloc(size * count);
-	ft_memset(new_space, 0, count);
+	if (!new_space)
+		return (0);
+	ft_bzero(new_space, size * count);
 	return (new_space);
 }
