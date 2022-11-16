5d7461e2b3888a605c29f005b4bee4d1cee6dad7
Jeheon Lee <jehelee@c2r5s1.42seoul.kr>
2022-11-16 12:27:57 +0900

    feat : ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jehelee <jehelee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 16:59:24 by jehelee           #+#    #+#             */
+/*   Updated: 2022/11/16 12:27:33 by jehelee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	check_space_sign(const char *string, int *sign)
+{
+	int	i;
+
+	i = 0;
+	while (string[i] >= 9 && string[i] <= 13 || string[i] == 32)
+			i++;
+	if (string[i] == '-' || string[i] == '+')
+	{
+		if (string[i] == '-')
+			*sign *= -1;
+		i++;
+	}
+	return (i);
+}
+
+int	ft_atoi(const char *string)
+{
+	int					sign;
+	int					i;
+	long long			result;
+	long long			max;
+
+	max = __LONG_MAX__;
+	sign = 1;
+	result = 0;
+	i = check_space_sign(string, &sign);
+	while (ft_isdigit(string[i]))
+	{
+		result = result * 10 + string[i] - '0' ;
+		if (sign == 1 && result >= max)
+			return (-1);
+		else if (sign == -1 && result >= max + 1)
+			return (0);
+		i++;
+	}
+	return (result * sign);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jehelee <jehelee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:21:58 by jehelee           #+#    #+#             */
-/*   Updated: 2022/11/15 16:54:39 by jehelee          ###   ########.fr       */
+/*   Updated: 2022/11/15 17:00:41 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int				ft_strncmp(const char *string1, const char *string2, size_t n);
 int				ft_memcmp(const void *string1, const void *string2, size_t n);
 int				ft_toupper(int c);
 int				ft_tolower(int c);
+int				ft_atoi(const char *string);
 size_t			ft_strlen(const char *string);
 size_t			ft_strlcat(char *dst, const char *src, size_t size);
 size_t			ft_strlcpy(char *dst, const char *src, size_t size);
