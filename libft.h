1204187f21be3d8d5d0a5c78a5a3bb5eecbb3412
Jeheon Lee <jehelee@c2r7s1.42seoul.kr>
2022-11-18 14:02:35 +0900

    <feat> : ft_strjoin prototype

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:21:58 by jehelee           #+#    #+#             */
-/*   Updated: 2022/11/16 18:21:51 by jehelee          ###   ########.fr       */
+/*   Updated: 2022/11/18 13:47:25 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,6 @@ char			*ft_strchr(const char *string, int c);
 char			*ft_strrchr(const char *string, int c);
 char			*ft_strnstr(const char *hay, const char *needle, size_t len);
 char			*ft_strdup(const char *string);
-
+char			*ft_substr(char const *string, unsigned int start, size_t len);
+char			*ft_strjoin(char const *string1, char const *string2);
 #endif
