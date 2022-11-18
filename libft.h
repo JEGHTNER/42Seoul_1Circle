c37c18ff5b0a1ebb0adfd361c935b444314eba35
Jeheon Lee <jehelee@c2r7s1.42seoul.kr>
2022-11-18 15:44:52 +0900

    <feat> : ft_split prototype

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:21:58 by jehelee           #+#    #+#             */
-/*   Updated: 2022/11/18 14:03:18 by jehelee          ###   ########.fr       */
+/*   Updated: 2022/11/18 14:25:14 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ char			*ft_strnstr(const char *hay, const char *needle, size_t len);
 char			*ft_strdup(const char *string);
 char			*ft_substr(char const *string, unsigned int start, size_t len);
 char			*ft_strjoin(char const *string1, char const *string2);
-char			*ft_strtrim(char const *s1, char const *set);
+char			*ft_strtrim(char const *string1, char const *trim_set);
+char			**ft_split(char const *string, char seperator);
 
 #endif
